9b4ec90916b07077721b322f9f018e8a69e5781d
Soda <hads2456@naver.com>
2018-12-12 10:31:08 +0900

    version_1_2018_12_11

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ANU_SF
+{
+    static class Program
+    {
+        static frmLogin login = null;
+        internal static bool logOK = false;
+
+        /// <summary>
+        /// 해당 응용 프로그램의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new frmMain());
+
+            #region
+            login = new frmLogin();
+            Application.Run(login);
+
+            if (logOK)
+            {
+                frmMain m = new frmMain();
+                //login.Close();
+                Application.Run(m);
+            }
+            #endregion
+        }
+    }
+}
